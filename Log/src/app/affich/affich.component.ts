cd7eaf135830daea7eb98a0ea8f2106413456b05
titchepaul <37667381+titchepaul@users.noreply.github.com>
2021-01-13 14:42:55 +0100

    firstlog

--- Log/src/app/affich/affich.component.ts
+++ Log/src/app/affich/affich.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { MyServiceService } from "../my-service.service";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { ForFilterService } from "../for-filter.service";
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ForFilterService } from '../for-filter.service';
 import { $ } from 'protractor';
 
 @Component({
-  selector: "app-affich",
-  templateUrl: "./affich.component.html",
-  styleUrls: ["./affich.component.css"],
+  selector: 'app-affich',
+  templateUrl: './affich.component.html',
+  styleUrls: ['./affich.component.css'],
 })
 export class AffichComponent implements OnInit {
-  private receive:  Object[];
+  private receive: Object[];
   private receivedata: Object[];
   private receivevalue: Object[];
   private colInfo: Observable<any>;
   private colInfo2 = [];
-  private counter : Object[];
-  private myCounter : number;
-  private flag : number;
-  //public share : Observable<any>;
+  private counter: Object[];
+  private myCounter: number;
+  private flag: number;
+  // public share : Observable<any>;
   myForm: FormGroup;
   formgroupobject = {};
 
@@ -41,113 +41,114 @@ export class AffichComponent implements OnInit {
     private filterService: ForFilterService,
     private router: Router,
     private route: ActivatedRoute
-  ) {  
+  ) {
   }
   data: string;
 
   ngOnInit() {
-    //if (this.service.name != null) {
+    // if (this.service.name != null) {
     //  localStorage.setItem("val", this.service.name);
-    //}
-    //this.isValid = true;
-    //this.data = localStorage.getItem("val");
+    // }
+    // this.isValid = true;
+    // this.data = localStorage.getItem("val");
 
-    //this.counter = this.service.getNumber;
-    let url = this.router.url;
-    this.data = url.substring(url.lastIndexOf("/")).replace("/","");
-    localStorage.setItem("val",this.data);
-    //console.log("number "+ this.service.getNumber.length);
+    // this.counter = this.service.getNumber;
+    const url = this.router.url;
+    this.data = url.substring(url.lastIndexOf('/')).replace('/','');
+    localStorage.setItem('val', this.data);
+    // console.log("number "+ this.service.getNumber.length);
     /*this.service.getTotalRows(this.data).toPromise().then(
       data => this.counter = data as number
     );*/
-    this.service.getTotalRows(localStorage.getItem("val")).toPromise().then(
+    this.service.getTotalRows(localStorage.getItem('val')).toPromise().then(
       data => this.counter = data as any[]
     );
-    this.colInfo = this.service.getInfoColumsTable(localStorage.getItem("val"));
+    this.colInfo = this.service.getInfoColumsTable(localStorage.getItem('val'));
 
-        this.service.getInfoColumsTable(localStorage.getItem("val")).toPromise().then(
-              data => {this.colInfo2 = data as any[]}
+    this.service.getInfoColumsTable(localStorage.getItem('val')).toPromise().then(
+              data => {this.colInfo2 = data as any[];}
         );
-        this.service.getAllColumnTable(localStorage.getItem("val")).toPromise().then(
+    this.service.getAllColumnTable(localStorage.getItem('val')).toPromise().then(
              data => this.receive = data as Object[]
         );
-        this.service.getDataInTableByRows(localStorage.getItem("val")).toPromise().then(
+    this.service.getDataInTableByRows(localStorage.getItem('val')).toPromise().then(
              data => this.receivedata = data as Object[]
         );
           /*this.service.getValueTable(localStorage.getItem("val")).toPromise().then(
              data => this.receivevalue = data as Object[]
           );*/
 
-        let subscription = this.colInfo.subscribe((data) => {
-            let formgroupobject = {};
-           //console.log('myData : '+  JSON.stringify(this.colInfo2));
+        // tslint:disable-next-line: prefer-const
+    let subscription = this.colInfo.subscribe((data) => {
+            const formgroupobject = {};
+           // console.log('myData : '+  JSON.stringify(this.colInfo2));
             data.forEach((el) => {
-              formgroupobject[el.name] = new FormControl("", [Validators.required]);
-              //console.log('elem : '+ JSON.stringify(el));
-              if (el.type === "datetime") {
-                formgroupobject[el.name + "Sign"] = new FormControl("", [
+              formgroupobject[el.name] = new FormControl('', [Validators.required]);
+              // console.log('elem : '+ JSON.stringify(el));
+              if (el.type === 'datetime') {
+                formgroupobject[el.name + 'Sign'] = new FormControl('', [
                   Validators.required,
                 ]);
               }
             });
             this.myForm = new FormGroup(formgroupobject);
-            console.log("okkkk");
+            console.log('okkkk');
           });
 
-    //this.counter = this.counter - 10;
+    // this.counter = this.counter - 10;
     this.flag = 1;
-    console.log("value :" + this.counter);
-    console.log("receive : " + localStorage.getItem("val"));
+    console.log('value :' + this.counter);
+    console.log('receive : ' + localStorage.getItem('val'));
     this.clearFilter();
 
   }
-  next(){
-    console.log("next "+this.counter);
-    //this.myCounter = this.counter as unknown as number;
-    console.log("count next "+this.myCounter);
-    if(this.flag === 1){
+  next() {
+    console.log('next '+ this.counter);
+    // this.myCounter = this.counter as unknown as number;
+    console.log('count next '+ this.myCounter);
+    if (this.flag === 1) {
       this.myCounter = this.counter as unknown as number;
       this.flag ++;
-      this.myCounter  = this.myCounter -2000;     //x2
-      if(this.myCounter >= 0){
-        //this.myCounter  = this.myCounter -20;
-        this.js_map.set("value",this.myCounter);
-        console.log("next counter " + this.myCounter.toString());
+      this.myCounter  = this.myCounter - 2000;     // x2
+      if (this.myCounter >= 0) {
+        // this.myCounter  = this.myCounter -20;
+        this.js_map.set('value', this.myCounter);
+        console.log('next counter ' + this.myCounter.toString());
         this.js_map.forEach((value, key) => {
           this.service.jsObject[key] = value;
         });
-        this.service.getDataInTableRowsOnly(localStorage.getItem("val"),this.service.jsObject).toPromise().then(
+        this.service.getDataInTableRowsOnly(localStorage.getItem('val'), this.service.jsObject).toPromise().then(
           data => this.receivedata = data as Object[]
         );
-      }else{
+      } else {
         this.myCounter = 0;
-        this.js_map.set("value",this.myCounter);
-        console.log("next counter " + this.myCounter.toString());
+        this.js_map.set('value', this.myCounter);
+        console.log('next counter ' + this.myCounter.toString());
         this.js_map.forEach((value, key) => {
           this.service.jsObject[key] = value;
         });
-        this.service.getDataInTableRowsOnly(localStorage.getItem("val"),this.service.jsObject).toPromise().then(
+        this.service.getDataInTableRowsOnly(localStorage.getItem('val'), this.service.jsObject).toPromise().then(
           data => this.receivedata = data as Object[]
         );
       }
-    }else{
+    } else {
       this.myCounter = this.myCounter - 1000;
-      if(this.myCounter > 0){
-        //this.myCounter = this.myCounter -10;
-        this.js_map.set("value",this.myCounter);
+      if (this.myCounter > 0) {
+        // this.myCounter = this.myCounter -10;
+        this.js_map.set('value', this.myCounter);
         this.js_map.forEach((value, key) => {
           this.service.jsObject[key] = value;
         });
-        this.service.getDataInTableRowsOnly(localStorage.getItem("val"),this.service.jsObject).toPromise().then(
+        this.service.getDataInTableRowsOnly(localStorage.getItem('val'), this.service.jsObject).toPromise().then(
           data => this.receivedata = data as Object[]
         );
-      }else{
+      } else {
         this.myCounter = 0;
-        this.js_map.set("value",this.myCounter);
+        this.js_map.set('value', this.myCounter);
         this.js_map.forEach((value, key) => {
           this.service.jsObject[key] = value;
         });
-        this.service.getDataInTableRowsOnly(localStorage.getItem("val"),this.service.jsObject).toPromise().then(
+        this.service.getDataInTableRowsOnly(localStorage.getItem('val'), this.service.jsObject).toPromise().then(
           data => this.receivedata = data as Object[]
         );
       }
@@ -193,64 +194,70 @@ export class AffichComponent implements OnInit {
     });*/
     this.map = new Map();
     this.filterService.jsonObject = {};
-    let sub = this.colInfo2.map(el =>{
-      if (el.type === "datetime") {
+    const sub = this.colInfo2.map(el => {
+      if (el.type === 'datetime') {
         this.up = el.name;
-        this.up_1 = this.myForm.get(this.up + "").value;
-        this.up_2 = this.myForm.get(this.up + "Sign").value;
+        this.up_1 = this.myForm.get(this.up + '').value;
+        this.up_2 = this.myForm.get(this.up + 'Sign').value;
         this.str_1 = el.name;
         this.str_2 = this.up_2 + this.up_1;
-        if (this.up_1 != "" && this.up_2 != "") {
-          console.log("name: " + this.str_1);
-          console.log("mes ups: " + this.str_2);
+        if (this.up_1 != '' && this.up_2 != '') {
+          console.log('name: ' + this.str_1);
+          console.log('mes ups: ' + this.str_2);
           this.map.set(this.str_1, this.str_2);
         }
       } else {
         this.up = el.name;
-        this.up_1 = this.myForm.get(this.up + "").value;
-        if (this.up_1 != "") {
+        this.up_1 = this.myForm.get(this.up + '').value;
+        if (this.up_1 != '') {
           this.map.set(this.up, this.up_1);
-          console.log("n fois : ");
+          console.log('n fois : ');
         }
       }
       this.map.forEach((value, key) => {
         this.filterService.jsonObject[key] = value;
       });
     });
-    if(!(Object.keys(this.filterService.jsonObject).length === 0)){
-      //this.receivevalue = this.service.getDataFilter(localStorage.getItem("val"),this.filterService.jsonObject);
-      this.service.getDataFilter(localStorage.getItem("val"),this.filterService.jsonObject).toPromise().then(
+    if (!(Object.keys(this.filterService.jsonObject).length === 0)) {
+      // this.receivevalue = this.service.getDataFilter(localStorage.getItem("val"),this.filterService.jsonObject);
+      this.service.getDataFilter(localStorage.getItem('val'), this.filterService.jsonObject).toPromise().then(
         data => this.receivedata = data as Object[]
      );
-      console.log("pour le filtre");
-      let tableName = this.route.snapshot.params['tableName'];
-      this.router.navigate(["table/"+tableName]);
+      console.log('pour le filtre');
+      const tableName = this.route.snapshot.params.tableName;
+      this.router.navigate(['table/'+ tableName]);
     }
   }
-  clearFilter(){
-        this.service.getDataInTableByRows(localStorage.getItem("val")).toPromise().then(
+  clearFilter() {
+        this.service.getDataInTableByRows(localStorage.getItem('val')).toPromise().then(
           data => this.receivedata = data as Object[]
         );
         this.myCounter = this.counter as unknown as number;
-        //this.myCounter = this.myCounter -500;
-        console.log("clear "+this.myCounter); 
-        //this.myForm.dirty; 
+        console.log('clear '+ this.myCounter);
+        // effacer le filtre
+        /*let control: AbstractControl = null;
+        this.myForm.reset();
+        this.myForm.markAsUntouched();
+        Object.keys(this.myForm.controls).forEach((key) =>{
+          control = this.myForm.controls[key];
+          control.setErrors(null);
+        });*/
   }
 
-  back(){
+  back() {
      this.myCounter = this.myCounter + 1000;
-     var offset = this.counter as unknown as number;
-      if(this.myCounter < offset){
-        this.js_map.set("value",this.myCounter);
+     let offset = this.counter as unknown as number;
+     if (this.myCounter < offset) {
+        this.js_map.set('value', this.myCounter);
         this.js_map.forEach((value, key) => {
           this.service.jsObject[key] = value;
         });
-        console.log("back "+this.myCounter);
-        this.service.getDataInTableRowsOnly(localStorage.getItem("val"),this.service.jsObject).toPromise().then(
+        console.log('back '+ this.myCounter);
+        this.service.getDataInTableRowsOnly(localStorage.getItem('val'), this.service.jsObject).toPromise().then(
           data => this.receivedata = data as Object[]
         );
-      }else{
-        this.myCounter = this.myCounter-1000;
+      } else {
+        this.myCounter = this.myCounter - 1000;
       }
   }
 }
--- Log/src/app/my-service.service.ts
+++ Log/src/app/my-service.service.ts
@@ -1,70 +1,72 @@
+// tslint:disable-next-line: quotemark
 import { Injectable } from "@angular/core";
-import {  HttpClient, HttpHeaders,HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { error } from "protractor";
+import {  HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { error } from 'protractor';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class MyServiceService {
   public name: string;
   public jsObject = {};
   myObject: {};
-  mySecurity = "abcdefg";
-  public hurt : any;
-   getNumber : object[];
+  mySecurity = 'abcdefg';
+  public hurt: any;
+   getNumber: object[];
   constructor(private http: HttpClient) {}
 
   public getData(name: string): Observable<any> {
-    console.log("récuperation contenu de la table");
+    console.log('récuperation contenu de la table');
     return this.http.get<any>(
       'https://localhost:44323/api/LogFile/GetData/' + name
     );
   }
-  public getDataInTableByRows(name: string): Observable<any>{
-    console.log("récupération des derniers enregistrement");
-    return this.http.get<any>('https://localhost:44323/api/LogFile/GetDataInTableByRows/'+ name);
+  public getDataInTableByRows(name: string): Observable<any> {
+    console.log('récupération des derniers enregistrement');
+    return this.http.get<any>('https://localhost:44323/api/LogFile/GetDataInTableByRows/' + name);
   }
-  public getTotalRows(name: string): Observable<any>{
-    console.log("renvoie du nombre total ");
-    return this.http.get<any>('https://localhost:44323/api/LogFile/GetTotalRowsValues/'+name);
+  public getTotalRows(name: string): Observable<any> {
+    console.log('renvoie du nombre total ');
+    return this.http.get<any>('https://localhost:44323/api/LogFile/GetTotalRowsValues/' + name);
   }
-  public getDataInTableRowsOnly(name: string, object: any): Observable<any>{
-    console.log("next enrégistrement " +name + "  value "+object);
-    return this.http.post<any>('https://localhost:44323/api/LogFile/GetDataInTableByRowsOnly/'+name,object);
+  public getDataInTableRowsOnly(name: string, object: any): Observable<any> {
+    console.log('next enrégistrement ' + name + '  value ' + object);
+    return this.http.post<any>('https://localhost:44323/api/LogFile/GetDataInTableByRowsOnly/' + name, object);
   }
   public getAllColumnTable(name: string): Observable<any> {
-    console.log("les colonnes ");
+    console.log('les colonnes ');
     return this.http.get<any>(
       'https://localhost:44323/api/LogFile/GetColTable/' + name
     );
   }
   public getAllTable_name(): Observable<any> {
-    console.log("recuperation data");
+    console.log('recuperation data');
     return this.http.get<any>(
       'https://localhost:44323/api/LogFile/GetAllTablesName'
     );
   }
   public getValueTable(tableName: string): Observable<any> {
-    console.log("récuperation contenu de la table");
+    console.log('récuperation contenu de la table');
     return this.http.get<any>(
       'https://localhost:44323/api/LogFile/GetAllDataInTable/' + tableName
     );
   }
   public getInfoColumsTable(name: string): Observable<any> {
-    console.log("les Infos des champs :");
+    console.log('les Infos des champs :');
     return this.http.get<any>(
       'https://localhost:44323/api/LogFile/GetAllColumsTable/' + name
     );
   }
   public getDataFilter(name: string, object: any): Observable<any> {
+    // tslint:disable-next-line: quotemark
     console.log("les filtres : " + name + JSON.stringify(object));
     console.log(object);
     this.myObject = JSON.stringify(object);
     const objectData = JSON.stringify(object);
     const usersJson: any[] = Array.of(objectData);
-    console.log("userJSON : " + usersJson);
-    console.log("myObject : " + this.myObject);
+    console.log('userJSON : ' + usersJson);
+    console.log('myObject : ' + this.myObject);
     return this.http.post<any>(
       'https://localhost:44323/api/LogFile/GetFilterData/' + name,
       object
